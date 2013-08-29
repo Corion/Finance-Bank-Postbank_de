cfb3ad2630d2b15e493d9db405c6082eca869b19
Max Maischein <corion@cpan.org>
2013-08-29 19:37:16 +0200

    Bump version

--- lib/Finance/Bank/Postbank_de.pm
+++ lib/Finance/Bank/Postbank_de.pm
@@ -12,7 +12,7 @@ use Encode qw(decode);
 
 use vars qw[ $VERSION ];
 
-$VERSION = '0.31';
+$VERSION = '0.32';
 
 BEGIN {
   Finance::Bank::Postbank_de->mk_accessors(qw( agent login password urls ));
