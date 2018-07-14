877db6fba7886988abc6df241f1d2151e5a7945a
Max Maischein <corion@corion.net>
2018-07-14 13:56:39 +0200

    Add ::Transaction

---
+++ lib/Finance/Bank/Postbank_de/APIv1/Transaction.pm
@@ -0,0 +1,38 @@
+package Finance::Bank::Postbank_de::APIv1::Transaction;
+use Moo;
+use Filter::signatures;
+no warnings 'experimental::signatures';
+use feature 'signatures';
+extends 'HAL::Resource';
+
+our $VERSION = '0.50';
+
+=head1 NAME
+
+Finance::Bank::Postbank_de::APIv1::Transaction - Postbank Transaction
+
+=head1 SYNOPSIS
+
+=cut
+
+has [ 'accountHolder', 'name', 'iban', 'currency', 'amount',
+      'productType',
+      'bookingDate', 'balance', 'usedTan', 'messages', 'transactionId',
+      'transactionType', 'purpose', 'transactionDetail',
+      'referenceInitials', 'reference', 'valutaDate'
+    ] => ( is => 'ro' );
+
+sub transactions_future( $self ) {
+    $self->fetch_resource_future( 'transactions' )->then(sub( $r ) {
+    my $tx = $account->fetch_resource( 'transactions' );
+        Future->done( map { Finance::Bank::Postbank_de::APIv1::Transaction->new }
+            @{ $r->_embedded->{transactionDTOList} })
+    );
+}
+    
+sub transactions( $self ) {
+    $self->transactions_future->get
+}
+    
+1;
+
