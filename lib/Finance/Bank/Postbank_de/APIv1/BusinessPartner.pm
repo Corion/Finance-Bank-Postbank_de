ff3e89bb96fbbe2a3a6fbd0a9d5bfcd3bf71bb92
Max Maischein <corion@corion.net>
2018-09-02 14:36:08 +0200

    Add author and bug tracker information to modules

--- lib/Finance/Bank/Postbank_de/APIv1/BusinessPartner.pm
+++ lib/Finance/Bank/Postbank_de/APIv1/BusinessPartner.pm
@@ -33,3 +33,36 @@ sub get_accounts( $self ) {
 
 1;
 
+=head1 AUTHOR
+
+Max Maischein, E<lt>corion@cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+L<perl>, L<WWW::Mechanize>.
+
+=head1 REPOSITORY
+
+The public repository of this module is
+L<https://github.com/Corion/Finance-Bank-Postbank_de>.
+
+=head1 SUPPORT
+
+The public support forum of this module is
+L<https://perlmonks.org/>.
+
+=head1 BUG TRACKER
+
+Please report bugs in this module via the RT CPAN bug queue at
+L<https://rt.cpan.org/Public/Dist/Display.html?Name=Finance-Bank-Postbank_de>
+or via mail to L<finance-bank-postbank_de-Bugs@rt.cpan.org>.
+
+=head1 COPYRIGHT (c)
+
+Copyright 2003-2018 by Max Maischein C<corion@cpan.org>.
+
+=head1 LICENSE
+
+This module is released under the same terms as Perl itself.
+
+=cut
