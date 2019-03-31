9580a6293ffde82eac44408b039f7618e0057399
Max Maischein <corion@corion.net>
2019-03-31 13:39:00 +0200

    Bump copyright year

--- lib/Finance/Bank/Postbank_de/APIv1/Account.pm
+++ lib/Finance/Bank/Postbank_de/APIv1/Account.pm
@@ -192,7 +192,7 @@ or via mail to L<finance-bank-postbank_de-Bugs@rt.cpan.org>.
 
 =head1 COPYRIGHT (c)
 
-Copyright 2003-2018 by Max Maischein C<corion@cpan.org>.
+Copyright 2003-2019 by Max Maischein C<corion@cpan.org>.
 
 =head1 LICENSE
 
